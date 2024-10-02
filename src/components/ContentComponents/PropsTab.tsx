15767e087c518088029ff1758a05b3db7f8f0ce3
Loan <129529306+lfantine@users.noreply.github.com>
2024-10-02 10:07:00 +0200

    tooltip fix

--- src/components/ContentComponents/PropsTab.tsx
+++ src/components/ContentComponents/PropsTab.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/cn";
 import { DivWithDescription, DivWithToolTip, isRequiredTitle, PropsCol, PropsColTitle, PropsColTitleText, PropsHeader, PropsHeaderBox, PropsHeaderTitle, PropsLine, PropsLine1_2, PropsLine2_2, PropsLineBox, PropsName, PropsNameWithDesc, requiredCol } from "@/lib/Style";
 import Separator from "../Separator";
 import { RadixPropDocumentation } from "@/app/(app)/components/[name]/page";
-
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "../ShadCn/Hover-Card";
 
 
 interface ProspTabProps {
@@ -13,11 +13,19 @@ interface ProspTabProps {
     return (
       <span className={cn(DivWithToolTip)}>
         <span className={cn("truncate" , outline ? "bg-gray-100 p-2 rounded-lg text-[#5b5e66]" : "")}>{name}<span className={cn(required === false ? "sm:hidden opacity-65" : "hidden")}>?</span></span>
-        <span className="group cursor-help">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"/><path d="M12 16v-4"/><path d="M12 8h.01"/></svg>
-            <div className={cn("hidden group-hover:block absolute bottom-full mb-2 p-2 text-wrap sm:text-nowrap top-4 sm:top-0 z-20 pointer-events-none text-center left-1/2 transform -translate-x-1/2 shadow-lg drop-shadow-sm shadow-gray-200 w-fit max-w-80 h-fit bg-white border border-gray-200 text-black rounded-md", tooDown && "-top-14")}>{tip}</div>
-        </span>
-    </span>
+        <HoverCard closeDelay={10} openDelay={5} >
+	          <div className="hover-card" >
+	                <HoverCardTrigger asChild className="relative" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"/><path d="M12 16v-4"/><path d="M12 8h.01"/></svg>
+                  </HoverCardTrigger>
+	                <HoverCardContent avoidCollisions className=" px-0 py-0 p-2.5 w-fit" collisionPadding={20} side="bottom" >
+	                    <span className="text-sm" >
+                          {addNewlineAfterPeriod(tip)}
+	                    </span>
+	                </HoverCardContent>
+	          </div>
+	      </HoverCard>
+      </span>
     )
   }
   
@@ -43,20 +51,28 @@ interface ProspTabProps {
                     {name}<span className={cn(required === false ? "sm:hidden opacity-65" : "hidden")}>?</span>
                 </p>
             </div>
-            <div className="group size-4 cursor-help">
-                <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"/><path d="M12 16v-4"/><path d="M12 8h.01"/></svg></span>
-                <div className={cn("hidden group-hover:block absolute z-20 bottom-full col-span-6 min-w-fit top-4 sm:top-0 text-wrap w-40 sm:w-80 p-2 text-xs transform -translate-x-1/4 lg:-translate-x-1/2 lg:left-1/2 shadow-lg drop-shadow-sm shadow-gray-200 text-left h-fit bg-white border border-gray-200 text-black rounded-md", tooDown && "-top-14")}>{addNewlineAfterPeriod(desc)}</div>
-            </div>
+            <HoverCard closeDelay={10} openDelay={5} >
+	            <div className="hover-card" >
+	                <HoverCardTrigger asChild className="relative" >
+                    <span><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"/><path d="M12 16v-4"/><path d="M12 8h.01"/></svg></span>
+	                </HoverCardTrigger>
+	                <HoverCardContent avoidCollisions className=" px-0 py-0 p-2.5" collisionPadding={20} side="bottom" >
+	                    <span className="text-sm" >
+                          {addNewlineAfterPeriod(desc)}
+	                    </span>
+	                </HoverCardContent>
+	            </div>
+	        </HoverCard>
         </div>
     //   <div className={cn(DivWithDescription)}><span className={cn(PropsNameWithDesc)}>{name}</span></div>
     )
   }
   
   function trueType(aType:string) {
-    if (aType.includes('|')) {
-      return withToolTip(aType, "enum", false, true, false);
-    } else if (aType.includes('=>')) {
+    if (aType.includes('=>')) {
       return withToolTip(aType, "function", false , true, false);
+    } else if (aType.includes('|')) {
+      return withToolTip(aType, "enum", false, true, false);
     } else return aType;
   }
   
